0e9a80299ac84376a509c59719a906b498042a07
Herpai Péter <herpaip@datapress.hu>
2022-10-31 17:07:51 +0100

    Add five different books total

--- src/test/java/hu/herpaipeter/DiscounterTest.java
+++ src/test/java/hu/herpaipeter/DiscounterTest.java
@@ -49,4 +49,11 @@ public class DiscounterTest {
         assertEquals(0.85 * 4 * discounter.getUnitPrice(),
                 discounter.getTotal(List.of(1, 2, 3, 4)));
     }
+
+    @Test
+    void five_different_books_20_percent_discount() {
+        Discounter discounter = new Discounter();
+        assertEquals(0.80 * 5 * discounter.getUnitPrice(),
+                discounter.getTotal(List.of(1, 2, 3, 4, 5)));
+    }
 }
