00c6208630b460503e17030bc41baa034161379a
Herpai Péter <herpaip@datapress.hu>
2022-10-31 17:04:00 +0100

    Refactor total calculation

--- src/main/java/hu/herpaipeter/Discounter.java
+++ src/main/java/hu/herpaipeter/Discounter.java
@@ -11,17 +11,17 @@ public class Discounter {
     }
     public double getTotal(List<Integer> items) {
         double total = items.size() * UNIT_PRICE;
-        if (items.size() == 2 && items.get(0) != items.get(1))
-            total *= 0.95;
-        if (items.size() == 3 && items.get(0) != items.get(1) &&
-                items.get(0) != items.get(2) && items.get(1) != items.get(2))
-            total *= 0.90;
-        if (items.size() == 4 &&
-                items.get(0) != items.get(1) && items.get(0) != items.get(2) && items.get(0) != items.get(3) &&
-                items.get(1) != items.get(2) && items.get(1) != items.get(3) &&
-                items.get(2) != items.get(3))
-            total *= 0.85;
+        if (1 < items.size() && isDifferent(items))
+            total *= (1.0 - (items.size() - 1) * 0.05);
         return total;
+    }
+
+    public boolean isDifferent(List<Integer> items) {
+        List<Integer> integers = items.stream().sorted().toList();
+        for (int i = 0; i < integers.size() - 1; ++i)
+            if (integers.get(i) == integers.get(i + 1))
+                return false;
+        return true;
     }
 
 }
